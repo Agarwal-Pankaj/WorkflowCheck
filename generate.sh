0cbf97293e36b755f86c810389efa29f091c8b78
Pankaj Agarwal <a.pankaj@1982@gmail.com>
2024-02-01 16:52:03 +0530

    update

--- generate.sh
+++ generate.sh
@@ -3,20 +3,12 @@
 npm install ts-node
 npx ts-node generate.ts
 
-
-function check_uncommitted_changes() {
-  status=$(git status --porcelain)
-  if [ -n "$status" ]; then
-    # status="${status//'%'/'%25'}"
-    # status="${status//$'\n'/'%0A'}"
-    # status="${status//$'\r'/'%0D'}"
-    echo -e "Following computed files are missing in PR\n"
+status=$(git status --porcelain)
+if [ -n "$status" ]; then
+    echo "Following computed files are missing in PR"
     echo "$status"
     exit 1
-  else
+else
     echo "Computed Files are up to date."
     exit 0
-  fi
-}
-
-echo $(check_uncommitted_changes)
+fi
