03db9a79fb9be269a9517b32ec5ab31087366e45
Pankaj Agarwal <a.pankaj@1982@gmail.com>
2024-02-06 20:45:00 +0530

    update

--- bin/generate.sh
+++ bin/generate.sh
@@ -3,12 +3,16 @@
 
 npx ts-node ../script/generate.ts
 
-status=$(git status --porcelain)
-if [ -n "$status" ]; then
-    echo "Following computed files are missing in PR"
-    echo "$status"
+if [ $? -eq 1 ]; then
     exit 1
 else
-    echo "Computed Files are up to date."
-    exit 0
+    status=$(git status --porcelain)
+    if [ -n "$status" ]; then
+        echo "Following computed files are missing in PR"
+        echo "$status"
+        exit 1
+    else
+        echo "Computed Files are up to date."
+        exit 0
+    fi
 fi
