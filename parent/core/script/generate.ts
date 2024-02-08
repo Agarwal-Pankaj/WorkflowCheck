ddb39bbae320debfd60fd85a11ed02efa850579f
Pankaj Agarwal <a.pankaj@1982@gmail.com>
2024-02-08 12:32:37 +0530

    update

--- parent/core/script/generate.ts
+++ parent/core/script/generate.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-fs.writeFileSync('./core/config/Sample.json5', 'two');
+fs.writeFileSync('./parent/core/config/Sample.json5', 'two');
 console.log('asdsadasdasdasdasdas')
 console.log('121212')
 // process.exit(1);
