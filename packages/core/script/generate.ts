1324b32ce2761bfbeed5c614910fb316fddae147
Pankaj Agarwal <a.pankaj@1982@gmail.com>
2024-02-09 18:07:19 +0530

    update

--- packages/core/script/generate.ts
+++ packages/core/script/generate.ts
@@ -1,7 +1,6 @@
 //import fs from 'fs';
 //import '@pankaj/flag'
-// const { featureFlagsConfigSchema } = require('@pankaj/flag');
-import featureFlagsConfigSchema  from "../../../shared/flag";
+const { featureFlagsConfigSchema } = require('@pankaj/flag');
 // fs.writeFileSync('Sample.json5', 'two');
 console.log('asdsadasdasdasdasdas')
 console.log('121212')
